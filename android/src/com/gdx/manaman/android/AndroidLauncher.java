1f60e7779125d2a05f011ab2c0510fc41921e8a1
sNAtttu <nsanteri@hotmail.com>
2015-08-05 19:19:27 +0300

    Den första commit

---
+++ android/src/com/gdx/manaman/android/AndroidLauncher.java
@@ -0,0 +1,16 @@
+package com.gdx.manaman.android;
+
+import android.os.Bundle;
+
+import com.badlogic.gdx.backends.android.AndroidApplication;
+import com.badlogic.gdx.backends.android.AndroidApplicationConfiguration;
+import com.gdx.manaman.ManamanGame;
+
+public class AndroidLauncher extends AndroidApplication {
+	@Override
+	protected void onCreate (Bundle savedInstanceState) {
+		super.onCreate(savedInstanceState);
+		AndroidApplicationConfiguration config = new AndroidApplicationConfiguration();
+		initialize(new ManamanGame(), config);
+	}
+}
---
+++ desktop/src/com/gdx/manaman/desktop/DesktopLauncher.java
@@ -0,0 +1,12 @@
+package com.gdx.manaman.desktop;
+
+import com.badlogic.gdx.backends.lwjgl.LwjglApplication;
+import com.badlogic.gdx.backends.lwjgl.LwjglApplicationConfiguration;
+import com.gdx.manaman.ManamanGame;
+
+public class DesktopLauncher {
+	public static void main (String[] arg) {
+		LwjglApplicationConfiguration config = new LwjglApplicationConfiguration();
+		new LwjglApplication(new ManamanGame(), config);
+	}
+}
